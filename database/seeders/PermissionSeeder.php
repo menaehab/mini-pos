0df9db20c6757d68830edc9fadb5ffcf89dfac9d
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2026-04-17 16:11:52 +0200

    feat: implement permission-based and seed admin user

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+
+        ];
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
